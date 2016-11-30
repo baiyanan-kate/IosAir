1ef061b12bf70557f751c5d9d319a35a621c02a9
eureka00zero <475032801@qq.com>
2016-11-30 09:23:37 -0800

    test ds

--- Air/Air/ConditionTableViewController.swift
+++ Air/Air/ConditionTableViewController.swift
@@ -16,7 +16,7 @@ class ConditionTableViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        // test
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
